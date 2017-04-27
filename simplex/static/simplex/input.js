7ed6580e2eac70cb966ff262fb88b69aa6125df9
es2wang <eddieshengyuwang@gmail.com>
2017-04-26 23:27:58 -0400

    ajax, matrix done

--- simplex/static/simplex/input.js
+++ simplex/static/simplex/input.js
@@ -2,20 +2,23 @@ $("form").submit(function(e){
 	e.preventDefault(); //prevent refresh
 	var rows = $('#rows').val();
 	var cols = $('#columns').val();
-
+	
 	$.ajax({
 		type:'POST',
-		url: '/simplex/insert-values/',
+		url: '/simplex/process-dimensions/',
 		data: {
 			rows: rows,
 			cols: cols,
 			csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
 		},
-		success: function(data) {
+		success: function(response) {
+			$("#second").html(response);
+			var matrixHeight = $('#matrix').height();
+			var xHeight = $('#x_var').height();
 
-			alert("Submitted");
-		}
-
-	});
+			var difference = (xHeight - matrixHeight)/2;
+			$('#x_var').css('top', difference + 'px');
+			}
+		});
     // alert("Submitted");
 });
